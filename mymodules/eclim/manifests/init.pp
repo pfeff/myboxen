19fe0e721628e88aedea65fca1e897ebaaa7dff3
Matt Pfefferle <mpfefferle@juniper.net>
2013-04-03 16:06:32 -0400

    Fix dependencies between eclipse and eclim

--- mymodules/eclim/manifests/init.pp
+++ mymodules/eclim/manifests/init.pp
@@ -1,7 +1,7 @@
 class eclim {
-  include eclipse
-  include java
-  include vim
+  require eclipse
+  require java
+  require vim
 
   $eclimUrl = "http://downloads.sourceforge.net/project/eclim/eclim/2.2.5/eclim_2.2.5.jar"
   $wgetEclim = "/usr/bin/wget $eclimUrl -P /tmp"
@@ -12,7 +12,6 @@ class eclim {
   }
 
   exec { "$installEclim":
-    require => File["$eclipse::home"],
     creates => "$eclipse::home/eclim"
   }
 
